e9ac51387e5d2fb1ca56197d9a329f9171d1af4e
Augusto Junior <48492543+augusto1jr@users.noreply.github.com>
2025-05-29 09:45:28 -0300

    add psicologo perfil info

--- front-end/app/paciente/psicologo/page.js
+++ front-end/app/paciente/psicologo/page.js
@@ -4,7 +4,6 @@ import { useRouter } from 'next/navigation';
 import styles from '../page.module.css';
 import AgendaPsicologo from '../home/AgendaPsicologo';
 
-
 export default function PerfilPsicologo() {
   const router = useRouter();
 
@@ -55,11 +54,12 @@ export default function PerfilPsicologo() {
 
   const [consultasPorPsicologo, setConsultasPorPsicologo] = useState({});
   const [avaliacoesPorPsicologo, setAvaliacoesPorPsicologo] = useState({});
+  
   const disponibilidadeMockada = [
-  { diaSemana: 'SEG', data: '26 MAI', horarios: ['15:00'] },
-  { diaSemana: 'TER', data: '27 MAI', horarios: ['15:00'] },
-  { diaSemana: 'QUA', data: '28 MAI', horarios: ['14:00', '15:00', '16:00'] },
-  { diaSemana: 'QUI', data: '29 MAI', horarios: ['18:00'] },
+    { diaSemana: 'SEG', data: '26 MAI', horarios: ['15:00'] },
+    { diaSemana: 'TER', data: '27 MAI', horarios: ['15:00'] },
+    { diaSemana: 'QUA', data: '28 MAI', horarios: ['14:00', '15:00', '16:00'] },
+    { diaSemana: 'QUI', data: '29 MAI', horarios: ['18:00'] },
   ];
 
   useEffect(() => {
@@ -71,16 +71,14 @@ export default function PerfilPsicologo() {
       return;
     }
     
-    // Dados do paciente
     fetch(`http://localhost:8080/pacientes/${pacienteId}`)
-    .then(res => res.json())
-    .then(data => setPaciente(data))
-    .catch(err => {
-      console.error('Erro ao buscar paciente:', err);
-      router.push('/app/login');
-    });
+      .then(res => res.json())
+      .then(data => setPaciente(data))
+      .catch(err => {
+        console.error('Erro ao buscar paciente:', err);
+        router.push('/app/login');
+      });
 
-    // Dados do psicólogo
     fetch(`http://localhost:8080/psicologos/${psicologoId}`)
       .then(res => res.json())
       .then(data => {
@@ -93,30 +91,27 @@ export default function PerfilPsicologo() {
       });
   }, []);
 
-    // Consultas e Avaliações de Psicólogos
   const fetchConsultasEAvaliacoes = (id) => {
-  // Consultas
-  fetch(`http://localhost:8080/psicologos/${id}/consultas`)
-    .then(res => res.json())
-    .then(data => {
-      setConsultasPorPsicologo(prev => ({
-        ...prev,
-        [id]: data
-      }));
-    })
-    .catch(err => console.error(err));
+    fetch(`http://localhost:8080/psicologos/${id}/consultas`)
+      .then(res => res.json())
+      .then(data => {
+        setConsultasPorPsicologo(prev => ({
+          ...prev,
+          [id]: data
+        }));
+      })
+      .catch(err => console.error(err));
 
-  // Avaliações
-  fetch(`http://localhost:8080/psicologos/${id}/avaliacoes`)
-    .then(res => res.json())
-    .then(data => {
-      setAvaliacoesPorPsicologo(prev => ({
-        ...prev,
-        [id]: data
-      }));
-    })
-    .catch(err => console.error(err));
-    };
+    fetch(`http://localhost:8080/psicologos/${id}/avaliacoes`)
+      .then(res => res.json())
+      .then(data => {
+        setAvaliacoesPorPsicologo(prev => ({
+          ...prev,
+          [id]: data
+        }));
+      })
+      .catch(err => console.error(err));
+  };
 
   const handleHome = () => {
     router.push('/paciente/home');
@@ -145,74 +140,91 @@ export default function PerfilPsicologo() {
             const avaliacoes = avaliacoesPorPsicologo[psicologo.id] || [];
 
             const totalAtendimentos = consultas.length;
-
-            const mediaAvaliacoes = 
+            const mediaAvaliacoes =
               avaliacoes.length > 0
                 ? (avaliacoes.reduce((sum, item) => sum + item.nota, 0) / avaliacoes.length).toFixed(1)
                 : 'N/A';
 
             return (
-              <div 
-                key={psicologo.id} 
-                className={styles.psicologoCard}
-              >
-                {/* Foto de Perfil */}
-                <img
-                  src={psicologo.foto || '/default-avatar.jpeg'}
-                  alt={`Foto de ${psicologo.nome}`}
-                  className={styles.psicologoFoto}
-                />
+              <div className={styles.psicologoContainer}>
+                {/* Card do Psicólogo */}
+                <div className={styles.psicologoCard}>
+                  <img
+                    src={psicologo.foto || '/default-avatar.jpeg'}
+                    alt={`Foto de ${psicologo.nome}`}
+                    className={styles.psicologoFoto}
+                  />
+                  <div className={styles.psicologoCardContent}>
+                    <div className={styles.psicologoInfo}>
+                      <h3>{psicologo.nome}</h3>
+                      <p className={styles.psicologoCrp}>CRP: {psicologo.crp}</p>
 
-                {/* Informações Básicas do Psicólogo */}
-                <div className={styles.psicologoCardContent}>
-                  <div className={styles.psicologoInfo}>
-                    <h3>{psicologo.nome}</h3>
-                    <p className={styles.psicologoCrp}>CRP: {psicologo.crp}</p>
+                      <div className={styles.chipsContainer}>
+                        {psicologo.especialidades.slice(0, 3).map((esp) => (
+                          <span key={esp} className={styles.chip}>{esp}</span>
+                        ))}
+                        {psicologo.abordagens.slice(0, 3).map((abo) => (
+                          <span key={abo} className={styles.chip}>{abo}</span>
+                        ))}
+                      </div>
 
-                    {/* Informações de Abordagens e Especialidades */}
-                    <div className={styles.chipsContainer}>
-                      {psicologo.especialidades.slice(0,3).map((esp) => (
-                        <span key={esp} className={styles.chip}>{esp}</span>
-                      ))}
-                      {psicologo.abordagens.slice(0,3).map((abo) => (
-                        <span key={abo} className={styles.chip}>{abo}</span>
-                      ))}
+                      <p className={styles.psicologoBio}>{psicologo.bio}</p>
+                      <p className={styles.psicologoValor}>
+                        {psicologo.valorConsulta} R$/50min{' '}
+                        {psicologo.aceitaBeneficio && (
+                          <span className={styles.valorSocial}>Valor Social Disponível</span>
+                        )}
+                      </p>
+                      <p className={styles.psicologoModalidade}>
+                        {psicologo.modalidadeAtendimento === 'REMOTO'
+                          ? 'Remoto'
+                          : psicologo.modalidadeAtendimento === 'PRESENCIAL'
+                          ? 'Presencial'
+                          : 'Híbrido'}
+                      </p>
+
+                      <div className={styles.psicologoStats}>
+                        <div className={styles.psicologoStat}>
+                          <span className="material-symbols-outlined starIcon">star</span>
+                          <strong>{mediaAvaliacoes}</strong>
+                          <span>({avaliacoes.length} comentários)</span>
+                        </div>
+                        <div className={styles.psicologoStat}>
+                          <span className="material-symbols-outlined">group</span>
+                          <strong>{totalAtendimentos}</strong>
+                          <span>atendimentos</span>
+                        </div>
+                      </div>
                     </div>
-
-                    {/* Informações de Atendimento */}
-                    <p className={styles.psicologoBio}>{psicologo.bio}</p>
-                    <p className={styles.psicologoValor}>
-                      {psicologo.valorConsulta} R$/50min{' '}
-                      {psicologo.aceitaBeneficio && (
-                        <span className={styles.valorSocial}>Valor Social Disponível</span>
-                      )}
-                    </p>
-                    <p className={styles.psicologoModalidade}>
-                      {psicologo.modalidadeAtendimento === 'REMOTO'
-                        ? 'Remoto'
-                        : psicologo.modalidadeAtendimento === 'PRESENCIAL'
-                        ? 'Presencial'
-                        : 'Híbrido'}
-                    </p>
-
-                    {/* Avaliações e Número de Consultas */}
-                    <div className={styles.psicologoStats}>
-                      <div className={styles.psicologoStat}>
-                        <span className="material-symbols-outlined starIcon">star</span>
-                        <strong>{mediaAvaliacoes}</strong>
-                        <span>({avaliacoes.length} comentários)</span>
-                      </div>
-                      <div className={styles.psicologoStat}>
-                        <span className="material-symbols-outlined">group</span>
-                        <strong>{totalAtendimentos}</strong>
-                        <span>atendimentos</span>
-                      </div>
+                    <div className={styles.agendaContainer}>
+                      <AgendaPsicologo disponibilidade={disponibilidadeMockada} />
                     </div>
                   </div>
+                </div>
 
-                  {/* Agenda do Psicólogo */}
-                  <div className={styles.agendaContainer}>
-                    <AgendaPsicologo disponibilidade={disponibilidadeMockada}/>
+                {/* Informações Extras */}
+                <div className={styles.psicologoInfoExtra}>
+                  <div className={styles.psicologosEspecialidades}>
+                    <h3>Especialidades do Psicólogo:</h3>
+                    <ul>
+                      {psicologo.especialidades.map((esp) => (
+                        <li key={esp}>{esp}</li>
+                      ))}
+                    </ul>
+                  </div>
+
+                  <div className={styles.psicologosAbordagens}>
+                    <h3>Abordagens que o Psicólogo trabalha:</h3>
+                    <ul>
+                      {psicologo.abordagens.map((abo) => (
+                        <li key={abo}>{abo}</li>
+                      ))}
+                    </ul>
+                  </div>
+
+                  <div className={styles.psicologosFormacao}>
+                    <h3>Formação do Psicólogo:</h3>
+                    <p>{psicologo.formacao}</p>
                   </div>
                 </div>
               </div>
@@ -221,17 +233,10 @@ export default function PerfilPsicologo() {
         ) : (
           <p>Carregando perfil...</p>
         )}
-
-        {/* Informações Extra do Psicólogo */}
-        <div className={styles.psicologoInfoExtra}>
-
-        </div>
-
       </div>
 
       {/* Barra Lateral */}
       <div className={styles.sideBar}>
-        {/* Perfil */}
         <div className={styles.profileBlock}>
           <img src={paciente.foto || '/default-avatar.jpeg'} alt="Foto de perfil" className={styles.avatar} />
           <div className={styles.profileInfo}>
@@ -242,43 +247,36 @@ export default function PerfilPsicologo() {
           </div>
         </div>
 
-        {/* Botão Home */}
         <button onClick={handleHome} className={styles.navButton}>
           <span className="material-symbols-outlined">home</span>
           <span>Início</span>
         </button>
 
-        {/* Botão Pesquisar */}
         <button className={styles.navButton}>
           <span className="material-symbols-outlined">search</span>
           <span>Pesquisar</span>
         </button>
 
-        {/* Botão Favoritos */}
         <button className={styles.navButton}>
           <span className="material-symbols-outlined">favorite</span>
           <span>Favoritos</span>
         </button>
 
-        {/* Botão Agenda */}
         <button className={styles.navButton}>
           <span className="material-symbols-outlined">calendar_month</span>
           <span>Agenda</span>
         </button>
 
-        {/* Botão Ajustes */}
         <button className={styles.navButton}>
           <span className="material-symbols-outlined">settings</span>
           <span>Ajustes</span>
         </button>
 
-        {/* Botão Ajuda */}
         <button className={styles.navButton}>
           <span className="material-symbols-outlined">help</span>
           <span>Ajuda</span>
         </button>
 
-        {/* Botão Sair */}
         <div className={styles.logoutBlock}>
           <button onClick={handleLogout} className={styles.logoutButton}>
             <span className="material-symbols-outlined">logout</span>Sair
@@ -286,7 +284,6 @@ export default function PerfilPsicologo() {
         </div>
       </div>
 
-      {/* Rodapé */}
       <footer className={styles.footer}>
         <p>PsiConnect &copy;</p>
       </footer>
