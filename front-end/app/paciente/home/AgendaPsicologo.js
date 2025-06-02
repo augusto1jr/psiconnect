61b1a24c476b92e8fd3c3b9366f2924063675657
Augusto Junior <48492543+augusto1jr@users.noreply.github.com>
2025-06-02 08:54:18 -0300

    add botao agendar in AgendaPsicologo.js

--- front-end/app/paciente/home/AgendaPsicologo.js
+++ front-end/app/paciente/home/AgendaPsicologo.js
@@ -1,23 +1,53 @@
+import { useState } from 'react';
 import styles from './AgendaPsicologo.module.css';
 
 const AgendaPsicologo = ({ disponibilidade }) => {
+  const [horarioSelecionado, setHorarioSelecionado] = useState(null);
+
+  const handleSelecionarHorario = (dia, hora) => {
+    setHorarioSelecionado({ dia, hora });
+  };
+
   return (
     <div className={styles.agendaContainer}>
-      {disponibilidade.map((dia, index) => (
-        <div key={index} className={styles.dia}>
-          <div className={styles.diaCabecalho}>
-            <p className={styles.diaSemana}>{dia.diaSemana}</p>
-            <p className={styles.diaData}>{dia.data}</p>
+      
+      <div className={styles.diasContainer}>
+        {disponibilidade.map((dia, index) => (
+          <div key={index} className={styles.dia}>
+            <div className={styles.diaCabecalho}>
+              <p className={styles.diaSemana}>{dia.diaSemana}</p>
+              <p className={styles.diaData}>{dia.data}</p>
+            </div>
+            <div className={styles.horarios}>
+              {dia.horarios.map((hora, i) => {
+                const isSelecionado =
+                  horarioSelecionado?.dia === dia.data &&
+                  horarioSelecionado?.hora === hora;
+
+                return (
+                  <div
+                    key={i}
+                    className={`${styles.horario} ${
+                      isSelecionado ? styles.horarioSelecionado : ''
+                    }`}
+                    onClick={() => handleSelecionarHorario(dia.data, hora)}
+                  >
+                    {hora}
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className={styles.horarios}>
-            {dia.horarios.map((hora, i) => (
-              <div key={i} className={styles.horario}>
-                {hora}
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
+
+      <button
+        className={styles.agendarButton}
+        disabled={!horarioSelecionado}
+      >
+        <span className="material-symbols-outlined">calendar_month</span>
+        <span>Agendar</span>
+      </button>
     </div>
   );
 };
